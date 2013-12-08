83d21c5290e6ac22e0f880fad5d00ed065030e0a
Johannes Zellner <webmaster@nebulon.de>
2013-12-07 23:11:38 -0800

    Use Redis for the backend to store app keys

--- client.js
+++ client.js
@@ -8,9 +8,10 @@ var request = require('superagent'),
 
 commander.version('0.1.0')
     .option('-s, --server [url]', 'Remote callback server.')
-    .option('add <route>', 'Add a new callback handler.')
-    .option('remove <route>', 'Removes a callback handler.')
-    .option('test <route>', 'Tests a callback handler.')
+    .option('-k, --key <appkey>', 'Application key to be recongnized by the callback server.')
+    .option('-p, --payload [payload]', 'Specifies an additional payload for the "test" commands.')
+    .option('refresh', 'Get a new the application key.')
+    .option('test <route>', 'Tests a callback route.')
     .option('listen [url]', 'Listen and waits for callbacks and dispatches them locally. Default to http://localhost:3000.')
     .parse(process.argv);
 
@@ -22,32 +23,38 @@ var server = commander.server || 'http://localhost:3001';
 debug('Using remote callback server %s', server);
 var listener = typeof commander.listen === 'string' ? commander.listen : 'http://localhost:3000';
 debug('Using %s to dispatch incoming requests locally.', listener);
-var userApiToken = 'dummy';
-debug('Using user api token %s.', userApiToken);
+var user = 'dummy';
+debug('Using user api token %s.', user);
+var appKey = commander.key;
+debug('Using app key "%s".', appKey);
+
 
 /*
- * program entry points
+ * actual functionality
  */
-function add(route) {
-    debug('Add callback', commander.add);
-
-}
-
-function remove(route) {
-    debug('Remove callback', commander.remove);
-
-}
-
 function test(route) {
     debug('Test callback', commander.test);
 
-    request.post(server + '/proxy/' + userApiToken + '/' + route).end(function (error, result) {
+    request.post(server + '/proxy/' + appKey + '/' + route).end(function (error, result) {
         if (error) {
             console.error('Unable to reach the server.', error);
             return;
         }
 
         console.log('Sent callback to %s. Status: %s', server, result.status);
+    });
+}
+
+function refresh() {
+    debug('Refresh callback');
+
+    request.get(server + '/appkey').end(function (error, result) {
+        if (error) {
+            console.error('Unable to reach the server.', error);
+            return;
+        }
+
+        console.log('Got new app key: "%s"', result.body.appkey);
     });
 }
 
@@ -68,21 +75,19 @@ function listen() {
         process.exit(1);
     });
 
-    socket.emit('auth', 'dummy');
+    socket.emit('auth', appKey);
 }
 
 
 /*
  * Main program flow starts here
  */
-if (commander.add) {
-    add(commander.add);
-} else if (commander.remove) {
-    remove(commander.remove);
-} else if (commander.test) {
+if (commander.test) {
     test(commander.test);
 } else if (commander.listen) {
     listen();
+} else if (commander.refresh) {
+    refresh();
 } else {
     commander.help();
 }
